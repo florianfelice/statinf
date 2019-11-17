30825ef2be1a3127e06c4b02fc0390ea91c4da17
Julien Virlogeux <40768558+Julien-Virl@users.noreply.github.com>
2019-11-17 16:59:41 +0100

    glm improvement suppress useless function

--- statinf/glm.py
+++ statinf/glm.py
@@ -15,7 +15,7 @@ Based on tutorial to be edited
 ## TODO: summary
 ## TODO: add dask
 
-class LogisticRegression:
+class Logit:
     def __init__(self, learning_rate=0.01, max_iter=1000, fit_intercept=True, verbose=False):
         self.lr = lr
         self.num_iter = num_iter
@@ -25,13 +25,10 @@ class LogisticRegression:
         intercept = np.ones((X.shape[0], 1))
         return np.concatenate((intercept, X), axis=1)
     
-    def __sigmoid(self, z):
+    def __logistic(self, z):
         return 1 / (1 + np.exp(-z))
 
-    def __loss(self, h, y):
-        return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
-
-    def log_likelihood(x, y, weights):
+    def __log_likelihood(x, y, weights):
         z = np.dot(x, weights)
         ll = np.sum( y*z - np.log(1 + np.exp(z)) )
         return ll
@@ -44,21 +41,11 @@ class LogisticRegression:
         self.theta = np.zeros(X.shape[1])
         
         for i in range(self.num_iter):
-            z = np.dot(X, self.theta)
             h = self.__sigmoid(z)
-            gradient = np.dot(X.T, (h - y)) / y.size
+            gradient = sum(y*log(h)+(1-y)*log(1-h))
             self.theta -= self.lr * gradient
             
             if(self.verbose == True and i % 10000 == 0):
                 z = np.dot(X, self.theta)
                 h = self.__sigmoid(z)
                 print(f'loss: {self.__loss(h, y)} \t')
-    
-    def predict_prob(self, X):
-        if self.fit_intercept:
-            X = self.__add_intercept(X)
-        return self.__sigmoid(np.dot(X, self.theta))
-    
-    def predict(self, X, threshold):
-        return self.predict_prob(X) >= threshold
-
