2a6492d7c7316ea927964fa07feee22de00f80d8
Florian Felice <admin@florianfelice.com>
2023-04-10 14:24:45 +0200

    Upload version 1.2.2 to pypi. Improved estimation of normalizing factor Z

--- statinf/distributions/discrete.py
+++ statinf/distributions/discrete.py
@@ -310,12 +310,12 @@ class CMPoisson(Discrete):
             assert self.lambda_ >= 0, ValueError('Value for parameter lambda must be strictly greater to 0 (lambda_ > 0)')
             assert self.nu_ >= 0, ValueError('Value for parameter nu must be greater or equal to 0 (nu_ >= 0)')
             warnings.filterwarnings("error")
-            self._Z = self.Z()
+            self._Z = self.Z(lambda_=self.lambda_, nu_=self.nu_, j=self.j, approx=True)
             warnings.resetwarnings()
 
-        self.nll_fun = lambda params, data, j: self.nloglike(params=params, data=data, j=j)
+        self.nll_fun = lambda params, data, j, approx: self.nloglike(params=params, data=data, j=j, approx=approx, Z=self.Z)
 
-    def Z(self, j=None) -> float:
+    def Z(self, lambda_, nu_, j=None, approx=False, log=False) -> float:
         """Compute the :math:`Z` factor, normalizing constant.
 
         The factor :math:`Z(\\lambda, \\nu)` serves as a normalizing constant such that the distribution satisfies the basic probability axioms (i.e. the probability mass function sums up to 1).
@@ -331,43 +331,89 @@ class CMPoisson(Discrete):
             Note that too large values for :obj:`j` will imply longer computation time and potential errors
             (:math:`j!` may become too large and might not fit in memory).
 
+        :param lambda\\_: First parameter :math:`\\lambda > 0` of the distribution
+        :type lambda\\_: :obj:`float`, optional
+        :param nu\\_: Second parameter :math:`\\nu > 0` of the distribution
+        :type nu\\_: :obj:`float`, optional
         :param j: Length of the sum for the normalizing constant, if :obj:`None` then we use the value from the :obj:`__init__` method, defaults to None
         :type j: :obj:`int`, optional
+        :param approx: Use approximation form for computing :math:`Z(\\lambda, \\nu)`, defaults to False
+        :type approx: :obj:`bool`
+        :param approx: Compute :math:`Z(\\lambda, \\nu)`, defaults to False
+        :type approx: :obj:`bool`
 
         :return: Z factor
         :rtype: :obj:`float`
         """
         j = j if j else self.j
-        z_i = 0
-        for i in range(j):
-            _dec = False
-            # We use a decimal placeholder variable in case we encounter overflow errors (number is too long).
-            # We don't use decimal as a default but only in case of error because it slows down computations,
-            # therefore, by default we use light and simple format and call Decimal when needed.
-            # For instances where we need Decimal, we will transform back the ratio of decimals to float as
-            # the value will be of reasonable size and not too long for a classical float.
 
-            # Compute the denominator
+        if (nu_ > 1 - self.eps) & (nu_ < 1 + self.eps):
+            # ## Poisson distribution
+            # When nu = 1, then we have a Poisson distribution
+            z_i = math.exp(lambda_)
+        elif (nu_ > 0) & (nu_ < 0 + self.eps):
+            # ## Geometric distribution
+            # When nu = 0, then we have a geometric distribution
+            z_i = 1 / (1 - lambda_)
+        elif (approx) & ((nu_ < 1 + self.eps) or (lambda_ > (10 ** nu_))):
+            # ## Approximation
+            # If we want the approximation and it verifies the assumptions (for nu > 1 and lambda > 10^nu), we use approximation
+            _dec = False
+            # Compute numerator
             try:
-                _denom = math.factorial(i) ** self.nu_
-            except OverflowError:
-                # If overflow error (i.e. output is too long), use Decimal format
-                _denom = Decimal(math.factorial(i)) ** Decimal(self.nu_)
-                _dec = True
-            # Compute the numerator
-            try:
-                _num = self.lambda_ ** i
+                _num = math.exp(nu_ * (lambda_ ** (1 / nu_)))
             except RuntimeWarning:
                 # If runtime warning it is similar to overflow error (i.e. output is too long), use Decimal format
-                _num = Decimal(self.lambda_) ** Decimal(i)
+                _pow = Decimal(lambda_) ** Decimal(1 / nu_)
+                _num = Decimal(math.exp(Decimal(nu_) * _pow))
+                _dec = True
+
+            # Compute denominator
+            try:
+                _denom = lambda_ ** ((nu_ - 1) / (2 * nu_))
+                _denom *= (2 * math.pi) ** ((nu_ - 1) / 2)
+                _denom *= math.sqrt(nu_)
+                _o_lambda = 1 / (lambda_ ** (1 / nu_))
+            except RuntimeWarning:
+                _denom = Decimal(lambda_) ** Decimal((nu_ - 1) / (2 * nu_))
+                _denom *= Decimal(2 * math.pi) ** Decimal((nu_ - 1) / 2)
+                _denom *= Decimal(math.sqrt(nu_))
+                _o_lambda = Decimal(1) / (Decimal(lambda_) ** Decimal(1 / nu_))
                 _dec = True
 
             if _dec:
                 _denom = Decimal(_denom)
                 _num = Decimal(_num)
-            z_i += float(_num / _denom)
+                _o_lambda = Decimal(_o_lambda)
+
+            # Compute the ratio
+            z_i = float(_num / _denom) * (1 + float(_o_lambda))
+        else:
+            # ## Almost exact form
+            z_i = - np.inf
+            z_last = 0
+            current_j = 0
+
+            while (np.abs(z_i - z_last) > self.eps) & (current_j < j):
+                z_last = z_i
+                _log_fact = np.sum(np.log(range(1, current_j)))
+                z_i = self.log_sum(z_i, current_j * math.log(lambda_) - nu_ * _log_fact)
+                current_j += 1
+
+            if not log:
+                z_i = math.exp(z_i)
 
         return float(np.sum(z_i))
+
+    def log_sum(self, x, y):
+        if x == -np.inf:
+            return y
+        elif y == -np.inf:
+            return x
+        elif x > y:
+            return x + math.log(1 + math.exp(y - x))
+        else:
+            return y + math.log(1 + math.exp(x - y))
 
     def pmf(self, x) -> float:
         """Computes the probability mass function for selected value :obj:`x`.
@@ -408,7 +454,7 @@ class CMPoisson(Discrete):
         return a * (1 / self._Z)
 
     @staticmethod
-    def nloglike(params, data, j=100) -> float:
+    def nloglike(params, data, Z, j=100, approx=False) -> float:
         """Static method to estumate the negative likelihood (used in :meth:`statinf.distributions.discrete.CMPoisson.fit` method).
 
         :formula: The log-likelihood function :math:`l` is defined by
@@ -421,6 +467,8 @@ class CMPoisson(Discrete):
         :type data: :obj:`numpy.array` or :obj:`list` or :obj:`pandas.Series`
         :param j: Length of the inifinite sum for the normalizing factor :math:`Z`, defaults to 100
         :type j: :obj:`int`, optional
+        :param approx: Use approximation form for computing :math:`Z(\\lambda, \\nu)`, defaults to False
+        :type approx: :obj:`bool`
 
         :return: Negative log-likelihood
         :rtype: :obj:`float`
@@ -429,49 +477,14 @@ class CMPoisson(Discrete):
         nu_ = params[1]
         X = np.asarray(data)
 
-        z_i = 0
-        for i in range(j):
-            _dec = False
-            # We use a decimal placeholder variable in case we encounter overflow errors (number is too long).
-            # We don't use decimal as a default but only in case of error because it slows down computations,
-            # therefore, by default we use light and simple format and call Decimal when needed.
-            # For instances where we need Decimal, we will transform back the ratio of decimals to float as
-            # the value will be of reasonable size and not too long for a classical float.
-
-            # Compute denominator
-            try:
-                # Try normal formula
-                _denom = math.factorial(i) ** nu_
-            except OverflowError:
-                # If overflow error (i.e. output is too long), use Decimal format
-                _denom = Decimal(math.factorial(i)) ** Decimal(nu_)
-                _dec = True
-
-            # Compute numerator
-            try:
-                _num = lambda_ ** i
-            except RuntimeWarning:
-                # If runtime warning it is similar to overflow error (i.e. output is too long), use Decimal format
-                _num = Decimal(lambda_) ** Decimal(i)
-                _dec = True
-
-            # If decimal was used
-            if _dec:
-                # If decimal was used for one of denominator or denominator, we apply decimal to all (for format consistency)
-                _denom = Decimal(_denom)
-                _num = Decimal(_num)
-
-            # Then compute the ratio and transform to float (whether decimal was used or not)
-            z_i += float(_num / _denom)
-
         # Compute log(Z)
-        log_Z = np.log(z_i)
+        log_Z = Z(lambda_=lambda_, nu_=nu_, j=j, approx=approx, log=True)
 
         _log_fact = np.asarray([math.log(math.factorial(_x)) for _x in X])
         ll = (math.log(lambda_) * np.sum(X)) - (nu_ * np.sum(_log_fact)) - (len(X) * log_Z)
         return -ll
 
-    def fit(self, data, method='L-BFGS-B', init_params=np.array([1., 1.]), j=None) -> dict:
+    def fit(self, data, method='L-BFGS-B', init_params=np.array([1., 1.]), j=None, approx=False) -> dict:
         """Estimates the parameters :math:`\\lambda` and :math:`\\nu` of the distribution from empirical data based on Maximum Likelihood Estimation.
 
         .. note::
@@ -490,17 +503,17 @@ class CMPoisson(Discrete):
         :rtype: obj:`dict`
         """
         # We transform warnings as error (for overflow) to handle in try / except: https://stackoverflow.com/questions/5644836/
-        warnings.filterwarnings("error")
         j = j if j else self.j
         bounds = [(self.eps, None), (self.eps, None)]
 
-        res = self._fit(data=data, bounds=bounds, method=method, init_params=init_params, args=(j,))
+        res = self._fit(data=data, bounds=bounds, method=method, init_params=init_params, args=(j, approx))
         self.lambda_ = res.x[0]
         self.nu_ = res.x[1]
-        self._Z = self.Z()
+        warnings.filterwarnings("error")
+        self._Z = self.Z(lambda_=self.lambda_, nu_=self.nu_, j=j, approx=approx)
         warnings.resetwarnings()
 
-        out = {'lambda_': self.lambda_, 'nu_': self.nu_}
+        out = {'lambda_': self.lambda_, 'nu_': self.nu_, 'convergence': res.success}
 
         if method in scipy_methods:
             out.update({'nll': self.nll})
