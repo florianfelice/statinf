c850579b3fe7a44587028c8625503c936ab2ecab
Florian Felice <admin@florianfelice.com>
2023-04-29 19:49:51 +0200

    Upload version 1.2.3 to pypi. Update fit + sample function for Conway-Maxwell

--- setup_new.py
+++ setup_new.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="statinf",
-    version="1.2.2",
+    version="1.2.3",
     author="Florian Felice",
     author_email="florian.felice@outlook.com",
     description="A library for statistics and causal inference",
--- statinf/distributions/discrete.py
+++ statinf/distributions/discrete.py
@@ -12,7 +12,7 @@ import warnings
 scipy_methods = ['BFGS', 'L-BFGS-B', 'Newton-CG', 'CG', 'Powell', 'Nelder-Mead']
 
 # Empty object with future attributes
-class Object(object):
+class _Object(object):
     pass
 
 
@@ -71,7 +71,7 @@ class Discrete:
         return [self._generate(_pmf, seed=_seed + i) for i in range(size)]
 
     def _fast_fit(self):
-        return ValueError('Fast or auto fit is not allowed for this distribution, please chose another value')
+        raise NotImplementedError('Fast or auto fit is not allowed for this distribution, please chose another value')
 
     def _fit(self, data, bounds=None, init_params=np.array([1]), verbose=False, method='auto', args=()):
         if method.lower() in ['auto', 'fast']:
@@ -110,7 +110,7 @@ class Poisson(Discrete):
         The distribution assumes equi-dispersion, meaning that :math:`\\mathbb{E}(X) = \\mathbb{V}(X)`.
 
         :param lambda\\_: Parameter :math:`\\lambda` representing the both location (:math:`\\mathbb{E}(X)`) and the scale (:math:`\\mathbb{V}(X)`) parameters, defaults to None
-        :type lambda\\_: :obj:`obj`, optional
+        :type lambda\\_: :obj:`float`, optional
 
         :example:
 
@@ -125,7 +125,7 @@ class Poisson(Discrete):
         ... {'lambda_': 2.46}
         >>> # The class stores the value of the estimated parameters
         >>> print(poiss.lambda_)
-        ... 2.76
+        ... 2.46
         >>> # So we can generate more samples using the fitted parameters
         >>> y = poiss.sample(200)
 
@@ -209,7 +209,7 @@ class Poisson(Discrete):
         :return: Estmated parameters
         :rtype: :obj:`dict`
         """
-        res = Object()
+        res = _Object()
         data = np.asarray(data)
         res.x = [data.mean()]
         return res
@@ -217,19 +217,20 @@ class Poisson(Discrete):
     def fit(self, data, method='fast', **kwargs) -> dict:
         """Estimates the parameter :math:`\\lambda` of the distribution from empirical data based on Maximum Likelihood Estimation.
 
-        The Maximum Likihood Estimator also corresponds to the emprical mean
+        The Maximum Likihood Estimator also corresponds to the emprical mean:
 
             .. math:: \\hat{\\lambda}_{\\text{MLE}} = \\dfrac{1}{n} \\sum_{i=1}^{n} x_i
 
-        The 'fast' :obj:`method` is available to estimate the parameter directly from the emprical mean.
+        The `method = 'fast'` is available to estimate the parameter directly from the emprical mean.
+        Any other value for the parameter `method` will use classical MLE.
 
         :param data: Data to fit and estimate parameters from.
         :type data: :obj:`numpy.array` or :obj:`list` or :obj:`pandas.Series`
         :param method: Optimization method to estimate the parameters as in the scipy library (allows 'fast' value), defaults to 'L-BFGS-B'
-        :type method: obj:`str`, optional
+        :type method: :obj:`str`, optional
 
         :return: Estimated parameter
-        :rtype: obj:`dict`
+        :rtype: :obj:`dict`
         """
         bounds = [(self.eps, None)]
         init_params = np.array([1])
@@ -246,7 +247,7 @@ class Poisson(Discrete):
 
 
 class CMPoisson(Discrete):
-    def __init__(self, lambda_=None, nu_=None, j=250, *args, **kwargs) -> None:
+    def __init__(self, lambda_=None, nu_=None, j=500, *args, **kwargs) -> None:
         """Conway-Maxwell Poisson distribution.
         This class allows to generate a random variable based on selected parameters and size but also to fit some data and estimate the parameters by means of Maximum Likelihood Estimation (MLE).
 
@@ -254,7 +255,7 @@ class CMPoisson(Discrete):
         (:class:`statinf.distributions.discrete.Poisson`).
         The distribution can handle non equi-dispersion cases where :math:`\\mathbb{E}(X) \\neq \\mathbb{V}(X)`.
         The level of dispersion is captured by :math:`\\nu` such that underdispersion is captured when :math:`\\nu > 1`,
-        equidispersions :math:`\\nu = 1` and overdispersion when :math:`\\nu < 1`.
+        equidispersion :math:`\\nu = 1` and overdispersion when :math:`\\nu < 1`.
 
         :formulae: The probability mass function (pmf) is defined by
 
@@ -262,7 +263,7 @@ class CMPoisson(Discrete):
 
             where :math:`Z(\\lambda, \\nu) = \\sum_{j=0}^{\\infty} \\dfrac{\\lambda^{j}}{(j!)^{\\nu}}` is calculated in :py:meth:`statinf.distributions.discrete.CMPoisson.Z`.
 
-        Special cases of the CMP distribution include well-known distributions
+        Special cases of the CMP distribution include well-known distributions.
 
             * When :math:`\\nu = 1`, one recovers the Poisson distribution with parameter :math:`\\lambda`
             * When :math:`\\nu = 0` and :math:`\\lambda < 1` one recovers the geometric distribution with parameter :math:`p = 1 - \\lambda` for the probability of success
@@ -310,12 +311,12 @@ class CMPoisson(Discrete):
             assert self.lambda_ >= 0, ValueError('Value for parameter lambda must be strictly greater to 0 (lambda_ > 0)')
             assert self.nu_ >= 0, ValueError('Value for parameter nu must be greater or equal to 0 (nu_ >= 0)')
             warnings.filterwarnings("error")
-            self._Z = self.Z(lambda_=self.lambda_, nu_=self.nu_, j=self.j, approx=True)
+            self._Z = self.Z(lambda_=self.lambda_, nu_=self.nu_, j=self.j)
             warnings.resetwarnings()
 
-        self.nll_fun = lambda params, data, j, approx: self.nloglike(params=params, data=data, j=j, approx=approx, Z=self.Z)
+        self.nll_fun = lambda params, data, j: self.nloglike(params=params, data=data, j=j, Z=self.Z)
 
-    def Z(self, lambda_, nu_, j=None, approx=False, log=False) -> float:
+    def Z(self, lambda_, nu_, j=None, log=False) -> float:
         """Compute the :math:`Z` factor, normalizing constant.
 
         The factor :math:`Z(\\lambda, \\nu)` serves as a normalizing constant such that the distribution satisfies the basic probability axioms (i.e. the probability mass function sums up to 1).
@@ -337,75 +338,31 @@ class CMPoisson(Discrete):
         :type nu\\_: :obj:`float`, optional
         :param j: Length of the sum for the normalizing constant, if :obj:`None` then we use the value from the :obj:`__init__` method, defaults to None
         :type j: :obj:`int`, optional
-        :param approx: Use approximation form for computing :math:`Z(\\lambda, \\nu)`, defaults to False
-        :type approx: :obj:`bool`
-        :param approx: Compute :math:`Z(\\lambda, \\nu)`, defaults to False
-        :type approx: :obj:`bool`
+        :param log: Compute :math:`\\log(Z(\\lambda, \\nu))`, defaults to False
+        :type log: :obj:`bool`
 
-        :return: Z factor
+        :return: Normalizing factor :math:`Z`
         :rtype: :obj:`float`
         """
         j = j if j else self.j
 
-        if (nu_ > 1 - self.eps) & (nu_ < 1 + self.eps):
-            # ## Poisson distribution
-            # When nu = 1, then we have a Poisson distribution
-            z_i = math.exp(lambda_)
-        elif (nu_ > 0) & (nu_ < 0 + self.eps):
-            # ## Geometric distribution
-            # When nu = 0, then we have a geometric distribution
-            z_i = 1 / (1 - lambda_)
-        elif (approx) & ((nu_ < 1 + self.eps) or (lambda_ > (10 ** nu_))):
-            # ## Approximation
-            # If we want the approximation and it verifies the assumptions (for nu > 1 and lambda > 10^nu), we use approximation
-            _dec = False
-            # Compute numerator
-            try:
-                _num = math.exp(nu_ * (lambda_ ** (1 / nu_)))
-            except RuntimeWarning:
-                # If runtime warning it is similar to overflow error (i.e. output is too long), use Decimal format
-                _pow = Decimal(lambda_) ** Decimal(1 / nu_)
-                _num = Decimal(math.exp(Decimal(nu_) * _pow))
-                _dec = True
+        # ## Almost exact form
+        z_i = - np.inf
+        z_last = 0
+        current_j = 0
 
-            # Compute denominator
-            try:
-                _denom = lambda_ ** ((nu_ - 1) / (2 * nu_))
-                _denom *= (2 * math.pi) ** ((nu_ - 1) / 2)
-                _denom *= math.sqrt(nu_)
-                _o_lambda = 1 / (lambda_ ** (1 / nu_))
-            except RuntimeWarning:
-                _denom = Decimal(lambda_) ** Decimal((nu_ - 1) / (2 * nu_))
-                _denom *= Decimal(2 * math.pi) ** Decimal((nu_ - 1) / 2)
-                _denom *= Decimal(math.sqrt(nu_))
-                _o_lambda = Decimal(1) / (Decimal(lambda_) ** Decimal(1 / nu_))
-                _dec = True
+        while (np.abs(z_i - z_last) > self.eps) & (current_j < j):
+            z_last = z_i
+            _log_fact = np.sum(np.log(range(1, current_j)))
+            z_i = self._log_sum(z_i, current_j * math.log(lambda_) - nu_ * _log_fact)
+            current_j += 1
 
-            if _dec:
-                _denom = Decimal(_denom)
-                _num = Decimal(_num)
-                _o_lambda = Decimal(_o_lambda)
-
-            # Compute the ratio
-            z_i = float(_num / _denom) * (1 + float(_o_lambda))
-        else:
-            # ## Almost exact form
-            z_i = - np.inf
-            z_last = 0
-            current_j = 0
-
-            while (np.abs(z_i - z_last) > self.eps) & (current_j < j):
-                z_last = z_i
-                _log_fact = np.sum(np.log(range(1, current_j)))
-                z_i = self.log_sum(z_i, current_j * math.log(lambda_) - nu_ * _log_fact)
-                current_j += 1
-
-            if not log:
-                z_i = math.exp(z_i)
+        if not log:
+            z_i = math.exp(z_i)
 
         return float(np.sum(z_i))
 
-    def log_sum(self, x, y):
+    def _log_sum(self, x, y):
         if x == -np.inf:
             return y
         elif y == -np.inf:
@@ -445,16 +402,13 @@ class CMPoisson(Discrete):
             _dec = True
 
         # Compute $(x!)^{\nu}$
-        _fact = [pow(math.factorial(_x), self.nu_) for _x in x]
-        if _dec:
-            # If decimal was used for power calculations, we have to use the same for factorial
-            _fact = [Decimal(p) for p in _fact]
+        _fact = [pow(np.exp(np.sum(np.log(range(1, _x)))), self.nu_) for _x in x]
 
         a = np.array([float(p / f) for p, f in zip(_pow, _fact)])
         return a * (1 / self._Z)
 
     @staticmethod
-    def nloglike(params, data, Z, j=100, approx=False) -> float:
+    def nloglike(params, data, Z, j=100) -> float:
         """Static method to estumate the negative likelihood (used in :meth:`statinf.distributions.discrete.CMPoisson.fit` method).
 
         :formula: The log-likelihood function :math:`l` is defined by
@@ -467,8 +421,6 @@ class CMPoisson(Discrete):
         :type data: :obj:`numpy.array` or :obj:`list` or :obj:`pandas.Series`
         :param j: Length of the inifinite sum for the normalizing factor :math:`Z`, defaults to 100
         :type j: :obj:`int`, optional
-        :param approx: Use approximation form for computing :math:`Z(\\lambda, \\nu)`, defaults to False
-        :type approx: :obj:`bool`
 
         :return: Negative log-likelihood
         :rtype: :obj:`float`
@@ -476,15 +428,16 @@ class CMPoisson(Discrete):
         lambda_ = params[0]
         nu_ = params[1]
         X = np.asarray(data)
+        n = len(X)
 
         # Compute log(Z)
-        log_Z = Z(lambda_=lambda_, nu_=nu_, j=j, approx=approx, log=True)
+        log_Z = Z(lambda_=lambda_, nu_=nu_, j=j, log=True)
 
         _log_fact = np.asarray([math.log(math.factorial(_x)) for _x in X])
-        ll = (math.log(lambda_) * np.sum(X)) - (nu_ * np.sum(_log_fact)) - (len(X) * log_Z)
+        ll = (math.log(lambda_) * np.sum(X)) - (nu_ * np.sum(_log_fact)) - (n * log_Z)
         return -ll
 
-    def fit(self, data, method='L-BFGS-B', init_params=np.array([1., 1.]), j=None, approx=False) -> dict:
+    def fit(self, data, method='L-BFGS-B', init_params='auto', j=None) -> dict:
         """Estimates the parameters :math:`\\lambda` and :math:`\\nu` of the distribution from empirical data based on Maximum Likelihood Estimation.
 
         .. note::
@@ -495,22 +448,25 @@ class CMPoisson(Discrete):
         :param data: Data to fit and estimate parameters from.
         :type data: :obj:`numpy.array` or :obj:`list` or :obj:`pandas.Series`
         :param method: Optimization method to estimate the parameters, defaults to 'L-BFGS-B'
-        :type method: obj:`str`, optional
+        :type method: :obj:`str`, optional
         :param init_params: Initial parameters for the optimization method, defaults to obj:`np.array([1., 1.])`
-        :type init_params: obj:`numpy.array`, optional
+        :type init_params: :obj:`numpy.array`, optional
 
         :return: Estimated parameters
-        :rtype: obj:`dict`
+        :rtype: :obj:`dict`
         """
         # We transform warnings as error (for overflow) to handle in try / except: https://stackoverflow.com/questions/5644836/
         j = j if j else self.j
         bounds = [(self.eps, None), (self.eps, None)]
+        if init_params == 'auto':
+            _disp = data.std() / data.mean()
+            init_params = np.array([1., _disp])
 
-        res = self._fit(data=data, bounds=bounds, method=method, init_params=init_params, args=(j, approx))
+        res = self._fit(data=data, bounds=bounds, method=method, init_params=init_params, args=(j,))
         self.lambda_ = res.x[0]
         self.nu_ = res.x[1]
         warnings.filterwarnings("error")
-        self._Z = self.Z(lambda_=self.lambda_, nu_=self.nu_, j=j, approx=approx)
+        self._Z = self.Z(lambda_=self.lambda_, nu_=self.nu_, j=j)
         warnings.resetwarnings()
 
         out = {'lambda_': self.lambda_, 'nu_': self.nu_, 'convergence': res.success}
